2d5c82795910fda0d604889741fc1d7769cdd895
Hinal Gandhi <hinal.g@simformsolutions.com>
2024-01-11 10:10:15 +0530

    Connected mongo server to our app

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
 const app = require("./app");
 
 console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
