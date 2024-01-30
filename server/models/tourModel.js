c9c24417b175d15f004d0ace79e95a0b2dcf739b
Hinal Gandhi <hinal.g@simformsolutions.com>
2024-01-30 18:25:52 +0530

    explored aggregation pipeline and diff middlewares of mongoose

--- server/models/tourModel.js
+++ server/models/tourModel.js
@@ -1,63 +1,112 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-    unique: true,
-    trim: true,
+const toursSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "A tour must have a rating"],
+      default: 4.5,
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a image cover"],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    secretTours: {
+      type: Boolean,
+      default: false,
+    },
   },
-  duration: {
-    type: Number,
-    required: [true, "A tour must have a duration"],
-  },
-  rating: {
-    type: Number,
-    required: [true, "A tour must have a rating"],
-    default: 4.5,
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, "A tour must have a group size"],
-  },
-  difficulty: {
-    type: String,
-    required: [true, "A tour must have a difficulty"],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, "A tour must have a description"],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, "A tour must have a image cover"],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+toursSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
 });
+
+//DOCUMENT MIDDLEWARE: runs before .save() and .create()
+toursSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  console.log(this);
+  next();
+});
+
+//QUERY MIDDLEWARE
+// toursSchema.pre("find", function (next) {
+toursSchema.pre(/^find/, function (next) {
+  this.find({ secretTours: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+toursSchema.post(/^find/, function (doc, next) {
+  console.log(`Query took ${Date.now() - this.start}ms`);
+  console.log(doc);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+toursSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secretTours: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
+// toursSchema.post("save", function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", toursSchema);
 
--- server/routes/tourRoutes.js
+++ server/routes/tourRoutes.js
@@ -8,12 +8,16 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = tourController;
 const router = express.Router();
 
 //param middleware
 // router.param("id", checkId);
 router.route("/").get(getAllTours).post(createTour);
+router.route("/get-stats").get(getTourStats);
+router.route("/monthly-plans/:year").get(getMonthlyPlan);
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
