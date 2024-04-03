8519241fd1137a44cd39e1b21313f96e7f0c932c
Hinal Gandhi <hinal.g@simformsolutions.com>
2024-04-03 16:09:29 +0530

    completed authentication and authorization part

--- server/app.js
+++ server/app.js
@@ -1,18 +1,51 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean")
+const hpp = require("hpp");
 
 const app = express();
 
-//1. Middlewares
+//1. Gloabl Middlewares
+app.use(helmet())
+
+//development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//limit requests from same API
+const limiter = rateLimit({
+  max:100,
+  windowMs : 60 * 60 * 1000,
+  message : "Too many requests from this IP, please try again in an hour!",
+})
+
+app.use('/api',limiter);
+
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const { whitelist } = require("validator");
 
-app.use(express.json());
+//Body parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+//Data sanitization against XSS
+app.use(xss())
+
+//Prevent parameter pollution
+app.use(hpp({
+  whitelist : ['duration','ratingsQuantity','ratingsAverage','maxGroupSize','difficulty','price']
+}))
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -20,6 +53,7 @@ app.use((req, res, next) => {
   next();
 });
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,10 +3,34 @@ const catchAsync = require("../utils/catchAsync");
 const User = require("../models/userModel");
 const appError = require("../utils/appError");
 
+
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+
+const createSendToken = (user,statusCode,res) => {
+  const token = signToken(user.id);
+
+  const cookieOptions = {
+    expiresIn : new Date(Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly : true
+  }
+
+  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  user.password = undefined;
+
+  res.cookie('jwt',token,cookieOptions)
+
+  res.status(statusCode).json({
+    status:'success',
+    token,
+    data : {
+      user
+    }
+  })
+}
 
 exports.signUp = catchAsync(async (req, res) => {
   const newUser = await User.create({
@@ -16,13 +40,7 @@ exports.signUp = catchAsync(async (req, res) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: "success",
-    token,
-    data: { user: newUser },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -40,11 +58,6 @@ exports.login = catchAsync(async (req, res, next) => {
     next(new appError("Incorrect email or password", 401));
   }
 
-  //3. If everything is ok,send token to client
-  const token = signToken(user._id);
-
-  res.status(200).json({
-    status: "success",
-    token,
-  });
+  // 3) If everything ok, send token to client
+  createSendToken(user, 200, res);
 });
